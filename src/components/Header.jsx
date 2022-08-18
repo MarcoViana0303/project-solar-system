482f17be3666f1500b15ec9e61dae534fac33622
Marco Viana <marcoviana100@gmail.com>
2022-08-18 17:21:59 -0300

    Req 01 check finalizado de verdade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<h1>Sistema Solar</h1>);
+    return (<header><h1>Sistema Solar</h1></header>);
   }
 }
 
